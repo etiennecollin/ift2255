f0fe93e25d151d5ed5dc029c16e347a9a47e16da
Etienne Collin <collin.etienne.contact@gmail.com>
2023-09-21 10:49:28 -0400

    Updated copyrights
    - Added student IDs
    - Gilles Dumas abandoned the course

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Utils.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Etienne Collin #20237904, Nicholas Cooper, Gilles Dumas, Charlotte Locas
+ * Copyright (c) 2023. Etienne Collin #20237904, Nicholas Cooper #20241729, Charlotte Locas #20211755
  */
 
 package com.etiennecollin.ift2255.clientCLI;
