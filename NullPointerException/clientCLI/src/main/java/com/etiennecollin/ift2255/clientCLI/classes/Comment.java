506db4986a2457d7d6fa5ec177f6e40a5de821cc
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-02 22:23:27 -0400

    Implemented Comment methods

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Comment.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Comment.java
@@ -3,5 +3,49 @@
  */
 
 package com.etiennecollin.ift2255.clientCLI.classes;
+
 public class Comment {
+    private String content;
+    private String title;
+    private Buyer author;
+    private int likes;
+
+    public Comment(String content, String title, Buyer author) {
+        this.setContent(content);
+        this.setTitle(title);
+        this.setAuthor(author);
+        this.setLikes(0);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Buyer getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Buyer author) {
+        this.author = author;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
 }
