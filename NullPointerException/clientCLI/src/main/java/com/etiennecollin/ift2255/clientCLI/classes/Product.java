7bb5b4573b35557b50d4cddb6d11d3e37f806c8c
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-02 22:23:32 -0400

    Implemented Product methods

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Product.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Product.java
@@ -4,5 +4,93 @@
 
 package com.etiennecollin.ift2255.clientCLI.classes;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Product {
+    private int price;
+    private Category category;
+    private String title; // Unique
+    private int likes;
+    private Seller seller;
+    private ArrayList<Comment> comments;
+    private Rating rating;
+
+    public Product(int price, String title, Category category) {
+        this.setPrice(price);
+        this.setTitle(title);
+        this.setCategory(category);
+        this.setLikes(0);
+        this.setComments(new ArrayList<>());
+        this.setRating(new Rating());
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return getTitle().equals(product.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
 }
