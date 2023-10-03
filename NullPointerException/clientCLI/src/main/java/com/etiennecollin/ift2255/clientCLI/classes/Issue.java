e32bd23242a3e63d3fc405f3a08a9ce969cc7a15
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-02 22:21:50 -0400

    Created basic classes
    These classes should be useful structuring the program.

---
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Issue.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2023. Etienne Collin #20237904, Nicholas Cooper #20241729, Charlotte Locas #20211755
+ */
+
+package com.etiennecollin.ift2255.clientCLI.classes;
+
+public class Issue {}
---
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Tuple.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023. Etienne Collin #20237904, Nicholas Cooper #20241729, Charlotte Locas #20211755
+ */
+
+package com.etiennecollin.ift2255.clientCLI.classes;
+
+public class Tuple<A, B> {
+    public A first;
+    public B second;
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
