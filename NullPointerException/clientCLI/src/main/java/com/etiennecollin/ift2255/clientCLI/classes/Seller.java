c884ae037fe499f86349a12978969ca139e03477
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-02 22:23:48 -0400

    Implemented Seller methods

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Seller.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Seller.java
@@ -4,5 +4,58 @@
 
 package com.etiennecollin.ift2255.clientCLI.classes;
 
+import java.util.Objects;
+
 public class Seller extends User{
+    private String name; // Unique
+    private String email;
+    private int phone;
+    private String address;
+
+    public Seller(String name, String email, int phone, String address) {
+        this.setName(name);
+        this.setEmail(email);
+        this.setPhone(phone);
+        this.setAddress(address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(getName(), seller.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
