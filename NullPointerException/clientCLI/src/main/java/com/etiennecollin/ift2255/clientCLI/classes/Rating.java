c3dbf6ce03a9c1464c4314197a5ccbfb13d8efcf
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-02 22:23:38 -0400

    Implemented Rating methods

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Rating.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Rating.java
@@ -4,5 +4,30 @@
 
 package com.etiennecollin.ift2255.clientCLI.classes;
 
+import java.util.NoSuchElementException;
+
 public class Rating {
+    private int rating; // Encoded as a percentage with no decimal places
+    private int ratingNumber;
+
+    public Rating() {
+        this.rating = 0;
+        this.ratingNumber = 0;
+    }
+
+    public int getRating() throws NoSuchElementException {
+        if (ratingNumber == 0) {
+            throw new NoSuchElementException("There are no ratings available yet.");
+        }
+        return rating;
+    }
+
+    public void addRating(int newRating) {
+        if (ratingNumber == 0) {
+            this.rating = newRating;
+        } else {
+            this.rating = (rating * ratingNumber + newRating) / (ratingNumber + 1);
+        }
+        ratingNumber++;
+    }
 }
