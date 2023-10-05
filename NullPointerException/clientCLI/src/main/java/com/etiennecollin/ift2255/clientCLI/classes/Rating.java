3c8ae3df17647217ae4a521f67e9fcdd9c11f069
Etienne Collin <collin.etienne.contact@gmail.com>
2023-10-05 16:17:47 -0400

    Fixed rating calculation

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Rating.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Rating.java
@@ -7,7 +7,13 @@ package com.etiennecollin.ift2255.clientCLI.classes;
 import java.util.NoSuchElementException;
 
 public class Rating {
-    private int rating; // Encoded as a percentage with no decimal places
+    /**
+     * Rating encoded as a percentage with no decimal places.
+     */
+    private int rating;
+    /**
+     * The number of reviews that generated the rating.
+     */
     private int ratingNumber;
 
     public Rating() {
@@ -22,12 +28,23 @@ public class Rating {
         return rating;
     }
 
-    public void addRating(int newRating) {
-        if (ratingNumber == 0) {
-            this.rating = newRating;
-        } else {
-            this.rating = (rating * ratingNumber + newRating) / (ratingNumber + 1);
+    public void addRating(int newRating) throws IllegalArgumentException {
+        if (newRating < 0 || newRating > 100) {
+            throw new IllegalArgumentException("Rating must be between 0 and 100 inclusively");
         }
+        this.rating = (rating * ratingNumber + newRating) / (ratingNumber + 1);
         ratingNumber++;
+    }
+
+    public void removeRating(int removedRating) throws IllegalArgumentException {
+        switch (ratingNumber) {
+            case 0:
+                throw new IllegalArgumentException("There are no ratings to remove");
+            case 1:
+                this.rating = 0;
+            default:
+                this.rating = (rating * ratingNumber - removedRating) / (ratingNumber - 1);
+        }
+        ratingNumber--;
     }
 }
