a4d7cb97c127703d51c236a28dd23466f11e7a3a
NICooper <nicholas.i.cooper@gmail.com>
2023-10-07 05:47:07 -0400

    Updated Seller menus and prettyMenu options

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Client.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Client.java
@@ -6,7 +6,6 @@
 package com.etiennecollin.ift2255.clientCLI;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static com.etiennecollin.ift2255.clientCLI.Utils.*;
 
@@ -21,10 +20,11 @@ public class Client {
      */
 
     //hardcoded for prototype
-    static String[] sellerMenu = {"Offer product", "Modify order status", "My activities", "Manage issues", "Confirm return reception", "Disconnect"};
+    static String[] sellerMenu = {"Offer product", "Modify order status", "Manage issues", "Update account information", "Disconnect"};
     static String[] buyerMenu = {"Catalog", "Search a product", "My cart", "My activities", "Find a seller", "My orders", "Disconnect"};
     static ArrayList<String> shoppingCart = new ArrayList<>();
     static ArrayList<String> likedProducts = new ArrayList<>();
+    static String[] categories = {"Books and manuals", "Learning ressources", "Stationery", "Hardware", "Office equipment"};
     static String[] productListDataBase = {"Computer", "Manual", "Utilities"};
     static String[] addToCartMenu = new String[productListDataBase.length+1];
     static ArrayList<String> sellersUsername = new ArrayList<>();
@@ -46,46 +46,24 @@ public class Client {
         System.out.println(prettify("Welcome to UniShop"));
 
         String[] loginMenu = {"Login", "Register"};
-        String answer = loginMenu[prettyMenu("Login menu", loginMenu)];
+        String answer = prettyMenu("Login menu", loginMenu);
+
+        UserRole userRole;
         if (answer.equals(loginMenu[0])) {
-            loginForm();
-        } else if (answer.equals(loginMenu[1])) {
-            createAccount();
+            userRole = loginForm();
+        } else {
+            userRole = createAccount();
         }
 
-        String[] roleChoice = {"Buyer", "Seller"};
-        String role = roleChoice[prettyMenu("Choose a menu to display", roleChoice)];
+        if (userRole == UserRole.Buyer) {
+            buyerMenu();
+        }
+        else if (userRole == UserRole.Seller) {
+            sellerMenu();
+        }
 
-        boolean disconnect = false;
-        do {
-            if(role.equals(roleChoice[0])){
-                int buyerAnswer = prettyMenu("Main menu", buyerMenu);
-                switch(buyerAnswer){
-                    case 0:
-                        displayCatalog();
-                        break;
-                    case 1:
-                        searchProduct();
-                        break;
-                    case 2:
-                        displayCart();
-                        break;
-                    case 3:
-                        displayActivities();
-                        break;
-                    case 4:
-                        findSeller();
-                        break;
-                    case 5:
-                        //displayOrders();
-                    case 6:
-                        disconnect = true;
-                }
-            }
-        } while(!disconnect);
-        System.out.println(prettify("You have been successfully disconnected"));
     }
-    private static void loginForm() {
+    private static UserRole loginForm() {
         System.out.println(prettify("Login menu"));
         while (true) {
             String username = prettyPrompt("Username");
@@ -98,17 +76,18 @@ public class Client {
             break;
         }
         System.out.println(prettify("Successfully logged in"));
+        return UserRole.Buyer;
     }
-    private static void createAccount() {
-        String[] choices = {"Buyer", "Seller"};
-        String accountType = choices[prettyMenu("Select your account type", choices)];
-        if (accountType.equals(choices[0])) {
+    private static UserRole createAccount() {
+        UserRole role = prettyMenu("Choose a menu to display", UserRole.class);
+
+        if (role == UserRole.Buyer) {
             buyerCreationForm();
-        } else if (accountType.equals(choices[1])) {
+        } else if (role == UserRole.Seller) {
             sellerCreationForm();
         }
 
-        System.out.println(prettify("Successfully registered"));
+        return role;
     }
     private static void buyerCreationForm() { // TODO return buyer
         String firstName = prettyPrompt("First name");
@@ -118,25 +97,74 @@ public class Client {
         String email = prettyPrompt("Email");
         String phoneNumber = prettyPrompt("Phone number");
         String address = prettyPrompt("Shipping address");
+
+        System.out.println(prettify("Successfully registered"));
     }
     private static void sellerCreationForm() { // TODO return seller
-        String firstName = prettyPrompt("First name");
-        String lastName = prettyPrompt("Last name");
         String username = prettyPrompt("Username");
         String password = prettyPrompt("Password");
         String email = prettyPrompt("Email");
         String phoneNumber = prettyPrompt("Phone number");
-        String product = prettyPrompt("Offered product"); // TODO create a product with createProduct()
+        String address = prettyPrompt("Shipping address");
+
+        System.out.println(prettify("Successfully registered"));
     }
+
+    public static void buyerMenu() {
+        boolean disconnect = false;
+        do {
+            int buyerAnswer = prettyMenuInt("Main menu", buyerMenu);
+            switch(buyerAnswer) {
+                case 0:
+                    displayCatalog();
+                    break;
+                case 1:
+                    searchProduct();
+                    break;
+                case 2:
+                    displayCart();
+                    break;
+                case 3:
+                    displayActivities();
+                    break;
+                case 4:
+                    findSeller();
+                    break;
+                case 5:
+                    //displayOrders();
+                case 6:
+                    disconnect = true;
+            }
+        } while(!disconnect);
+        System.out.println(prettify("You have been successfully disconnected"));
+    }
+
+    public static void sellerMenu() {
+        boolean disconnect = false;
+
+        do {
+            int answer = prettyMenuInt("Main menu", sellerMenu);
+            switch (answer) {
+                case 0 -> addProduct();
+                case 1 -> changeOrderStatus();
+                case 2 -> manageProblems();
+                case 3 -> updateSellerInfo();
+                case 4 -> disconnect = true;
+            }
+        } while(!disconnect);
+
+        System.out.println(prettify("You have been successfully disconnected"));
+    }
+
     private static void displayCatalog(){
         String[] catChoice = {"Books and manuals", "Learning ressources", "Stationery", "Hardware", "Office equipment", "Main menu"};
         int choice=0;
 
         while(choice!=5){ //5 is the option for Main Menu
-            choice = prettyMenu("Categories", catChoice);
+            choice = prettyMenuInt("Categories", catChoice);
             //for prototype only
 
-            int addToCart = prettyMenu("Add to cart", addToCartMenu);
+            int addToCart = prettyMenuInt("Add to cart", addToCartMenu);
 
             if(addToCart==(addToCartMenu.length-1)){ //back option
                 continue;
@@ -162,11 +190,11 @@ public class Client {
         }
         searchResult.add("Main menu");
         //display result
-        int choice = prettyMenu("Resultats for \"" + keyWord + "\"", searchResult);
+        int choice = prettyMenuInt("Resultats for \"" + keyWord + "\"", searchResult);
 
         if(!(choice==searchResult.size()-1)){
             String productMenu[] = {"Like the product", "Add to cart", "Main Menu"};
-            int answer = prettyMenu(searchResult.get(choice), productMenu);
+            int answer = prettyMenuInt(searchResult.get(choice), productMenu);
 
             switch(answer){
                 case 0:
@@ -185,7 +213,7 @@ public class Client {
     //For prototype only
     private static void findSeller() {
         String[] searchBy = {"Name", "Address", "Article category"};
-        int search = prettyMenu("Search by", searchBy);
+        int search = prettyMenuInt("Search by", searchBy);
 
         switch(search){
             case 0: //Name
@@ -198,14 +226,14 @@ public class Client {
                 break;
             case 2: //Article category
                 String[] catChoice = {"Books and manuals", "Learning ressources", "Stationery", "Hardware", "Office equipment", "Main menu"};
-                int cat = prettyMenu("Categories", catChoice);
+                int cat = prettyMenuInt("Categories", catChoice);
                 break;
         }
         displaySellers();
     }
     public static void displaySellers(){
         for(String seller : sellersUsername)
-        System.out.println(prettify(seller));
+            System.out.println(prettify(seller));
     }
     public static void displayCart(){
         System.out.println(prettify("My cart: "));
@@ -224,6 +252,72 @@ public class Client {
             System.out.println("Your order has been placed successfully");
         }
     }
+
+    public static void addProduct() {
+        String title = prettyPrompt("Title");
+        String category = prettyMenu("Category", categories);
+        String description = prettyPrompt("Description");
+        String brandName = prettyPrompt("Brand name");
+        String modelName = prettyPrompt("Model name");
+        int quantity = prettyPromptInt("Quantity");
+        float price = prettyPromptFloat("Price");
+
+        System.out.println("Product " + title + " added!");
+    }
+
+    public static void changeOrderStatus() {
+        String[] orderMenu = new String[]{"#123 - Pending Seller", "Main menu"};
+        int orderIdx = prettyMenuInt("Select the order to update", orderMenu);
+        if (orderIdx == orderMenu.length - 1) { // last index returns to main menu
+            return;
+        }
+
+        String[] orderStatusMenu = new String[]{"In transit", "Main menu"};
+        int statusIdx = prettyMenuInt("Select new status", orderStatusMenu);
+        if (statusIdx == orderStatusMenu.length - 1) { // last index returns to main menu
+            return;
+        }
+
+        String shippingCompany = prettyPromptValidated("Shipping company", (s) -> !s.equals(""));
+        String trackingId = prettyPromptValidated("Tracking ID", (s) -> !s.equals(""));
+
+        System.out.println("Order status updated!");
+    }
+
+    public static void manageProblems() {
+        String[] problemList = new String[]{"#123 - Order not received", "Main menu"};
+
+        int problemIdx = prettyMenuInt("Select a problem", problemList);
+        if (problemIdx == problemList.length - 1) {
+            return;
+        }
+
+        System.out.println("#123 - Order not received");
+        System.out.println("I put in an order 5 minutes ago and I still have not received it!!!");
+
+        String[] solutionTypes = new String[]{"Refund", "Replace", "Repair"};
+        String solution = prettyMenu("Select a solution", solutionTypes);
+        String description = prettyPrompt("Provide solution details to the buyer");
+        // set new problem status
+
+        System.out.println("Problem response sent!");
+    }
+
+    public static void updateSellerInfo() {
+        String[] updateInfoMenu = new String[]{"Password", "Email", "Phone number", "Shipping address", "Main menu"};
+        while (true) {
+            int menuIdx = prettyMenuInt("Select the information you'd like to change", updateInfoMenu);
+
+            switch (menuIdx) {
+                case 0 -> prettyPrompt("Set a new password");
+                case 1 -> prettyPrompt("Set a new email address");
+                case 2 -> prettyPrompt("Set a new phone number");
+                case 3 -> prettyPrompt("Set a new shipping address");
+                case 4 -> { return; }
+            }
+        }
+    }
+
     //General metrics
     public static void displayActivities(){
         System.out.println(prettify("My activities:"));
--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Utils.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Utils.java
@@ -5,7 +5,9 @@
 package com.etiennecollin.ift2255.clientCLI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Function;
 
 /**
  * The `Utils` class provides utility methods for common tasks in the client command-line interface (CLI).
@@ -73,6 +75,42 @@ public class Utils {
         return scanner.next().strip();
     }
 
+    protected static String prettyPromptValidated(String prompt, Function<String, Boolean> validator) {
+        while (true) {
+            System.out.print(prettify(prompt) + ": ");
+            String answer = scanner.next().strip();
+            if (validator.apply(answer)) {
+                return answer;
+            }
+
+            System.out.println("Invalid response. Please follow the prompt.");
+        }
+    }
+
+    protected static int prettyPromptInt(String prompt) {
+        while (true) {
+            System.out.print(prettify(prompt) + ": ");
+            try {
+                return Integer.parseInt(scanner.next().strip());
+            } catch (NumberFormatException ignored) {
+                System.out.println("Please enter a whole number with no thousands symbol.");
+            }
+        }
+
+    }
+
+    protected static float prettyPromptFloat(String prompt) {
+        while (true) {
+            System.out.print(prettify(prompt) + ": ");
+            try {
+                return Float.parseFloat(scanner.next().strip());
+            } catch (NumberFormatException ignored) {
+                System.out.println("Please enter a number with no thousands symbol.");
+            }
+        }
+
+    }
+
     /**
      * Displays a menu with choices to the user and expects a numeric selection.
      * It keeps prompting until a valid selection is made.
@@ -82,7 +120,7 @@ public class Utils {
      *
      * @return The index of the selected choice (0-based index).
      */
-    protected static int prettyMenu(String prompt, String[] choices) {
+    protected static int prettyMenuInt(String prompt, String[] choices) {
         // Instantiate the variables used to store the answer and its parsed version
         String answer;
         int answerParsed;
@@ -94,8 +132,6 @@ public class Utils {
             menu = menu.concat(prettify(i + ") " + choice + "\n"));
             i++;
         }
-
-
 
         // Repeat until input is valid
         while (true) {
@@ -124,46 +160,61 @@ public class Utils {
         }
     }
 
-    //for ArrayLists
-    protected static int prettyMenu(String prompt, ArrayList<String> choices) {
-        // Instantiate the variables used to store the answer and its parsed version
-        String answer;
-        int answerParsed;
+    /**
+     * Displays a menu with choices to the user and expects a numeric selection.
+     * It keeps prompting until a valid selection is made.
+     *
+     * @param prompt  The message to display as the prompt.
+     * @param choices An ArrayList of strings representing the menu choices.
+     *
+     * @return The index of the selected choice (0-based index).
+     */
+    protected static int prettyMenuInt(String prompt, ArrayList<String> choices) {
+        return prettyMenuInt(prompt, choices.toArray(new String[0]));
+    }
 
-        // Generate the menu containing all choices
-        String menu = "";
-        int i = 0;
-        for (String choice : choices) {
-            menu = menu.concat(prettify(i + ") " + choice + "\n"));
-            i++;
+    /**
+     * Displays a menu with choices to the user and expects a numeric selection.
+     * It keeps prompting until a valid selection is made.
+     *
+     * @param prompt  The message to display as the prompt.
+     * @param choices An array of strings representing the menu choices.
+     *
+     * @return The String associated with the selected choice.
+     */
+    protected static String prettyMenu(String prompt, String[] choices) {
+        return choices[prettyMenuInt(prompt, choices)];
+    }
+
+    /**
+     * Displays a menu with choices to the user and expects a numeric selection.
+     * It keeps prompting until a valid selection is made.
+     *
+     * @param prompt  The message to display as the prompt.
+     * @param choices An ArrayList of strings representing the menu choices.
+     *
+     * @return The String associated with the selected choice.
+     */
+    protected static String prettyMenu(String prompt, ArrayList<String> choices) {
+        return choices.get(prettyMenuInt(prompt, choices.toArray(new String[0])));
+    }
+
+    /**
+     * Displays a menu with choices to the user and expects a numeric selection.
+     * It keeps prompting until a valid selection is made.
+     *
+     * @param prompt  The message to display as the prompt.
+     * @param enumClass The class of the Enum used to represent choices.
+     *
+     * @return The Enum associated with the selected choice.
+     */
+    protected static <T extends Enum<T>> T prettyMenu(String prompt, Class<T> enumClass) {
+        var enumConstants = enumClass.getEnumConstants();
+        ArrayList<String> enumNames = new ArrayList<>();
+        for (var c: enumConstants) {
+            enumNames.add(c.name());
         }
 
-
-
-        // Repeat until input is valid
-        while (true) {
-            // Print menu and prompt
-            System.out.println("------------");
-            System.out.println(prettify(prompt) + ": ");
-            System.out.print(menu);
-            System.out.print(prettify("Selection: "));
-            answer = scanner.next().strip();
-
-            // Parse answer
-            try {
-                answerParsed = Integer.parseInt(answer);
-            } catch (NumberFormatException e) {
-                System.out.println(prettify("Invalid input"));
-                continue;
-            }
-
-            // Check that answer corresponds to a choice
-            if (answerParsed < 0 || answerParsed >= choices.size()) {
-                System.out.println(prettify("Invalid input"));
-                continue;
-            }
-
-            return answerParsed;
-        }
+        return Enum.valueOf(enumClass, prettyMenu(prompt, enumNames));
     }
 }
