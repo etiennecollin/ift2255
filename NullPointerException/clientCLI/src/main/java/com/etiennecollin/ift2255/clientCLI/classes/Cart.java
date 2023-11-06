659a39d01b6a4ed19ff93d4009d32e5ac31ba22c
Etienne Collin <collin.etienne.contact@gmail.com>
2023-11-06 18:36:13 -0500

    Update Cart.java
    - Modified the addProduct/subtractProduct methods which now take a quantity as an argument
    - Added a way to create an order from the cart

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Cart.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/classes/Cart.java
@@ -12,35 +12,35 @@ public class Cart {
      * Total value of cart in cents.
      */
     private int totalPrice;
+    private int totalFidelityPoints;
 
-    public ArrayList<Tuple<Product, Integer>> getProducts() {
-        return products;
+    public Cart() {
+        this.totalPrice = 0;
+        this.totalFidelityPoints = 0;
     }
 
-    public int getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void addProduct(Product product) {
+    public void addProduct(Product product, int quantity) {
         for (Tuple<Product, Integer> tuple : this.products) {
             if (tuple.first == product) {
-                totalPrice += tuple.first.getPrice();
-                tuple.second++;
+                totalPrice += product.getPrice();
+                totalFidelityPoints += product.getFidelityPoints();
+                tuple.second += quantity;
                 return;
             }
         }
-        this.products.add(new Tuple<>(product, 1));
+        this.products.add(new Tuple<>(product, quantity));
     }
 
-    public void subtractProduct(Product product) throws IllegalArgumentException {
+    public void subtractProduct(Product product, int quantity) throws IllegalArgumentException {
         for (int i = 0; i < this.products.size(); i++) {
             Tuple<Product, Integer> tuple = this.products.get(i);
             if (tuple.first == product) {
-                totalPrice -= tuple.first.getPrice();
-                if (tuple.second <= 1) {
+                totalPrice -= product.getPrice();
+                totalFidelityPoints -= product.getFidelityPoints();
+                if (tuple.second <= quantity) {
                     this.products.remove(i);
                 } else {
-                    tuple.second--;
+                    tuple.second -= quantity;
                 }
                 break;
             }
@@ -52,11 +52,36 @@ public class Cart {
         for (int i = 0; i < this.products.size(); i++) {
             Tuple<Product, Integer> tuple = this.products.get(i);
             if (tuple.first == product) {
-                totalPrice -= tuple.first.getPrice() * tuple.second;
+                totalPrice -= product.getPrice() * tuple.second;
+                totalFidelityPoints -= product.getFidelityPoints() * tuple.second;
                 this.products.remove(i);
                 break;
             }
         }
         throw new IllegalArgumentException("Product not in cart.");
+    }
+
+    public Order createOrder(String email, int phone, String address, String billingAddress, String creditCardName, int creditCardNumber, int creditCardExp, int creditCardSecretDigits) {
+        Order order = new Order(this.getTotalPrice(), this.getTotalFidelityPoints(), this.getProducts(), email, phone, address, billingAddress, creditCardName, creditCardNumber, creditCardExp, creditCardSecretDigits);
+        emptyCart();
+        return order;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int getTotalFidelityPoints() {
+        return totalFidelityPoints;
+    }
+
+    public ArrayList<Tuple<Product, Integer>> getProducts() {
+        return products;
+    }
+
+    public void emptyCart() {
+        this.products.clear();
+        this.totalFidelityPoints = 0;
+        this.totalPrice = 0;
     }
 }
