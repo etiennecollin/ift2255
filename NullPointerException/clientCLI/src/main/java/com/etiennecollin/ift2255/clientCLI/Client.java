19caa431bc4e46643d8cd704439239415693115f
Etienne Collin <collin.etienne.contact@gmail.com>
2023-11-12 16:43:06 -0500

    Moved UserRole

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Client.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/Client.java
@@ -372,6 +372,13 @@ public class Client {
             System.out.println(prettify(seller));
         }
     }
+
+    /**
+     * Temporary for prototype.
+     */
+    public enum UserRole {
+        Buyer, Seller
+    }
 }
 
 
