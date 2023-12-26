7bc4b75d419ab0cfdef4550b95370b35312173d3
Etienne Collin <collin.etienne.contact@gmail.com>
2023-12-26 16:48:16 -0500

    JavaDoc and formatting for controllers

--- NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/controllers/TicketController.java
+++ NullPointerException/clientCLI/src/main/java/com/etiennecollin/ift2255/clientCLI/controllers/TicketController.java
@@ -18,42 +18,96 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Controller class responsible for handling operations related to tickets.
+ */
 public class TicketController {
+    /**
+     * The view renderer to display views.
+     */
     private final ViewRenderer renderer;
+    /**
+     * The ticketing model to interact with ticket data.
+     */
     private final TicketingModel ticketModel;
 
+    /**
+     * Constructs a new TicketController.
+     *
+     * @param renderer    The view renderer to display views.
+     * @param ticketModel The ticketing model to interact with ticket data.
+     */
     public TicketController(ViewRenderer renderer, TicketingModel ticketModel) {
         this.renderer = renderer;
         this.ticketModel = ticketModel;
     }
 
+    /**
+     * Displays the details of a specific ticket.
+     *
+     * @param ticketId The UUID of the ticket to display.
+     */
     public void displayTicket(UUID ticketId) {
         renderer.addNextView(new TicketDisplay(ticketId, this, UniShop.getInstance().getProfileController()), true);
     }
 
+    /**
+     * Displays a menu with a list of tickets.
+     */
     public void displayTickets() {
         renderer.addNextView(new TicketsMenu(this, UniShop.getInstance().getProfileController()), true);
     }
 
+    /**
+     * Retrieves a specific ticket by its UUID.
+     *
+     * @param ticketId The UUID of the ticket to retrieve.
+     *
+     * @return The ticket with the specified UUID.
+     */
     public Ticket getTicket(UUID ticketId) {
         return ticketModel.getTicket(ticketId);
     }
 
+    /**
+     * Retrieves a list of tickets based on the user's role (Buyer or Seller).
+     *
+     * @return A list of tickets associated with the user.
+     */
     public List<Ticket> getTickets() {
         UUID userId = Session.getInstance().getUserId();
 
         if (Session.getInstance().getUserType() == UserType.Buyer) {
             return ticketModel.getTickets((ticket) -> ticket.getBuyerId() == userId);
-        }
-        else {
+        } else {
             return ticketModel.getTickets((ticket) -> ticket.getSellerId() == userId);
         }
     }
 
+    /**
+     * Creates a return shipment for a ticket.
+     *
+     * @param ticketId        The UUID of the ticket for which to create a return shipment.
+     * @param trackingNumber  The tracking number of the return shipment.
+     * @param deliveryDate    The delivery date of the return shipment.
+     * @param shippingCompany The shipping company used for the return shipment.
+     *
+     * @return The result of the operation (success or failure).
+     */
     public OperationResult createReturnShipment(UUID ticketId, String trackingNumber, LocalDate deliveryDate, String shippingCompany) {
         return ticketModel.createReturnShipment(ticketId, trackingNumber, deliveryDate, shippingCompany);
     }
 
+    /**
+     * Creates a replacement shipment for a ticket.
+     *
+     * @param ticketId        The UUID of the ticket for which to create a replacement shipment.
+     * @param trackingNumber  The tracking number of the replacement shipment.
+     * @param deliveryDate    The delivery date of the replacement shipment.
+     * @param shippingCompany The shipping company used for the replacement shipment.
+     *
+     * @return The result of the operation (success or failure).
+     */
     public OperationResult createReplacementShipment(UUID ticketId, String trackingNumber, LocalDate deliveryDate, String shippingCompany) {
         return ticketModel.createReplacementShipment(ticketId, trackingNumber, deliveryDate, shippingCompany);
     }
